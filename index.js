39b943227fb4a91c82d8eb8cba451caf53bed4cd
MHayes <mhayes0022@gmail.com>
2024-03-21 22:29:03 -0500

    Added basic file structure

---
+++ index.js
@@ -0,0 +1,40 @@
+//runs the application w/ input from .lib/
+const inquirer = require('inquirer');
+const fs = require('fs');
+//should be a third to connect shapes.js
+
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Please input up to three characters',
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What color would you like those characters to be?',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Please choose a shape:',
+        choices: ['Triangle', 'Square', 'Circle'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'What color would you like your shape to be?',
+    },
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, error => {
+        if (error) {
+            return console.log(error);
+        }
+        else {
+            console.log("Generated logo.svg");
+        }
+    })
+};
